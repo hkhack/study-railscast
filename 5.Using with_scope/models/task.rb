ecbda5d5381806e13e9964995ff7e9549795d16e
hkhack <30777575+hkhack@users.noreply.github.com>
2018-01-07 16:29:19 +0800

    Update task.rb

--- 5.Using with_scope/models/task.rb
+++ 5.Using with_scope/models/task.rb
@@ -3,3 +3,6 @@ def self.find_incomplete(options = {})
     find_all_by_complete(false, :order => 'created_at DESC')
   end
 end
+
+
+# with_scope 详情见 https://apidock.com/rails/ActiveRecord/Base/with_scope/class
